aeb33f69c9cda664464c94cf5fdf0daf4a1ba3c9
Kar-Wai See <karwai@gmail.com>
2018-11-13 13:57:54 +1100

    EF code first db creation via migration

---
+++ CustomerManagement/Migrations/20181113025509_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ShoppingApp.Services.CustomerManagement.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AddressLine1 = table.Column<string>(nullable: true),
+                    AddressLine2 = table.Column<string>(nullable: true),
+                    Contact = table.Column<string>(nullable: true),
+                    Country = table.Column<string>(nullable: true),
+                    CreatedDate = table.Column<DateTime>(nullable: false),
+                    CreditLimit = table.Column<decimal>(nullable: false),
+                    CreditOwe = table.Column<decimal>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    ModifiedDate = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Postcode = table.Column<int>(nullable: false),
+                    State = table.Column<string>(nullable: true),
+                    Status = table.Column<int>(nullable: false),
+                    Suburb = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
