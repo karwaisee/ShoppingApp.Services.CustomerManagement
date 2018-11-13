1cc83e3b68094192640c278a7cbc07f12373e473
Kar-Wai See <karwai@gmail.com>
2018-11-13 13:43:59 +1100

    Default namespace and models

---
+++ CustomerManagement/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ShoppingApp.Services.CustomerManagement.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public CustomerStatus Status { get; set; }
+        public decimal CreditLimit { get; set; }
+        public decimal CreditOwe { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string Suburb { get; set; }
+        public int Postcode { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+
+    public enum CustomerStatus { Good = 1, InDebt = 2 };
+}
